68462916b2e6af80a17190e3ba04c354cdfad19c
TheSayelMunsi <76156817+TheSayelMunsi@users.noreply.github.com>
2023-02-21 18:26:30 +0530

    Update serializers.py

--- assignment/serializers.py
+++ assignment/serializers.py
@@ -20,4 +20,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password')
-        # extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {'password': {'write_only': True}}
